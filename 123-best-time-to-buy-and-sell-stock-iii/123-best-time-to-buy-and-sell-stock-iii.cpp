9d55e2f8437f3813251dd05f759304ff64f50372
Arpit Srivastav <66209332+arpit-15s@users.noreply.github.com>
2022-04-28 21:01:49 +0530

    Time: 1143 ms (6.55%), Space: 78.4 MB (58.07%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> profit(n, 0);
+        int max_profit = 0;
+        int min_price = prices[0];
+        for(int i = 1; i < n; i++){
+            if(min_price > prices[i]){
+                min_price = prices[i];
+            }
+            max_profit = max(max_profit, prices[i] - min_price);
+            profit[i] = max_profit;
+        }
+        int max_price = prices[n - 1];
+        max_profit = 0;
+        for(int i: profit){
+            cout << i << " ";
+        }
+        cout << endl;
+        for(int i = n - 2; i >= 0; i--){
+            if(max_price < prices[i]){
+                max_price = prices[i];
+            }
+            max_profit = max(max_profit, max_price - prices[i]);
+            profit[i] += max_profit;
+        }
+        
+        for(int i: profit){
+            cout << i << " ";
+        }
+        cout << endl;
+        return *max_element(profit.begin(), profit.end());
+    }
+};
