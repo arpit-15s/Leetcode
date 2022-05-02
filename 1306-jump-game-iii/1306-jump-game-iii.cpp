2b3e6475d445d60e5011f4f3e7bfce04e21bab10
Arpit Srivastav <66209332+arpit-15s@users.noreply.github.com>
2022-05-02 12:52:49 +0530

    Time: 77 ms (27.34%), Space: 35.6 MB (31.42%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,28 +1,11 @@
 class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
-        if(arr[start] == 0) return true;
         int n = arr.size();
-        vector<bool> flag(n, false);
-        queue<int> q;
-        q.push(start);
-        flag[start] = true;
-        while(!q.empty()){
-            int top = q.front();
-            q.pop();
-            int right = top + arr[top];
-            int left = top - arr[top];
-            if(right < n && flag[right] == false){
-                if(arr[right] == 0) return true;
-                q.push(right);
-                flag[right] = true;
-            }
-            if(left >= 0 && flag[left] == false){
-                if(arr[left] == 0) return true;
-                q.push(left);
-                flag[left] = true;
-            }
-        }
-        return false;
+        if(start >= n || start < 0 || arr[start] == -1) return false;
+        if(arr[start] == 0) return true;
+        int temp = arr[start];
+        arr[start] = -1;
+        return canReach(arr, start + temp) | canReach(arr, start - temp);
     }
 };
