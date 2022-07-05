0aef2eaaac9e160238a1c9f07fb354b9523a696c
Arpit Srivastav <66209332+arpit-15s@users.noreply.github.com>
2022-07-05 16:22:10 +0530

    Time: 167 ms (5.06%), Space: 17.8 MB (41.00%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -2,31 +2,21 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        int candy = n, i=1;
-        while(i<n){
-            if(ratings[i] == ratings[i-1]){
-                i++;
-                continue;
-            }
-            
-            //For increasing slope
-            int peak = 0;
-            while(ratings[i] > ratings [i-1]){
-                peak++;
-                candy += peak;
-                i++;
-                if(i == n) return candy;
-            }
-            
-            //For decreasing slope
-            int valley = 0;
-            while(i<n && ratings[i] < ratings[i-1]){
-                valley++;
-                candy += valley;
-                i++;
-            }
-            candy -= min(peak, valley); //Keep only the higher peak
+        vector<int> dp(n, 1);
+        for(int i = 1; i < n; i++){
+            if(ratings[i] > ratings[i - 1])
+                dp[i] = dp[i - 1] + 1;
         }
-        return candy;
+        int res = 0;
+        for(int i = n - 2; i >= 0; i--){
+            if(ratings[i] > ratings[i + 1]){
+                dp[i] = max(dp[i], dp[i + 1] + 1);
+            }
+            res += dp[i];
+        }
+        for(int i: dp)
+            cout << i << " ";
+        cout << endl;
+        return res + dp[n - 1];
     }
 };
