9835a9e806a9aefba0c551cd219cf04d55e6f4eb
Arpit Srivastav <66209332+arpit-15s@users.noreply.github.com>
2022-08-04 18:28:20 +0530

    Time: 41 ms (77.93%), Space: 24.7 MB (5.69%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,13 +1,40 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int> pq;
-        for(vector<int> i: matrix){
-            for(int j: i){
-                pq.push(j);
-                if(pq.size() > k) pq.pop();
-            }
+    int binSearch(vector<int> nums, int k){
+        int start = 0, end = nums.size() - 1;
+        if(nums[end] <= k) return nums.size();
+        if(nums[0] > k) return 0;
+        while(start != end){
+            int mid = start + (end - start) / 2;
+            if(nums[mid] <= k && nums[mid + 1] > k) return mid + 1;
+            else if(nums[mid] <= k) start = mid + 1;
+            else end = mid;
         }
-        return pq.top();
+        return 0;
+    }
+    
+    int findLesserCount(vector<vector<int>>& matrix, int k){
+        int n = matrix.size();
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            count += binSearch(matrix[i], k);
+        }
+        return count;
+    }
+    
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        int start = matrix[0][0];
+        int end = matrix[n - 1][n - 1];
+        while(start != end){
+            int mid = start + (end - start) / 2;
+            int count = findLesserCount(matrix, mid);
+            cout << mid << endl;
+            if(count < k)
+                start = mid + 1;
+            else 
+                end = mid;
+        }
+        return start;
     }
 };
