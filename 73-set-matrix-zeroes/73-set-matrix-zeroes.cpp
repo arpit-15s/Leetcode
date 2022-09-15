9be2143e0661ac137258f4eba9dca013b63e430b
Arpit Srivastav <66209332+arpit-15s@users.noreply.github.com>
2022-09-15 20:46:19 +0530

    Time: 34 ms (17.58%), Space: 13.2 MB (81.95%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -5,36 +5,39 @@ public:
         int n = matrix[0].size();
         bool row = false, col = false;
         for(int i = 0; i < m; i++){
-            if(matrix[i][0] == 0) row = true;
+            if(!matrix[i][0]){
+                col = true;
+                break;
+            }
         }
-        for(int j = 0; j < n; j++){
-            if(matrix[0][j] == 0) col = true;
+        for(int i = 0; i < n; i++){
+            if(!matrix[0][i]){
+                row = true;
+                break;
+            }
         }
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
-                if(matrix[i][j] == 0){
+                if(!matrix[i][j]){
                     matrix[0][j] = 0;
                     matrix[i][0] = 0;
                 }
             }
         }
-        
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
-                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                if(!matrix[i][0] || !matrix[0][j]){
                     matrix[i][j] = 0;
                 }
             }
         }
-        if(row == true){
-            for(int i = 0; i < m; i++){
-                matrix[i][0] = 0;
-            }
+        if(row){
+            for(int i = 0; i < n; i++)
+                matrix[0][i] = 0;
         }
-        if(col == true){
-            for(int j = 0; j < n; j++){
-                matrix[0][j] = 0;
-            }
+        if(col){
+            for(int i = 0; i < m; i++)
+                matrix[i][0] = 0;
         }
         return;
     }
